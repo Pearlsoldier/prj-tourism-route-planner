0e4eccd7e15bf9f352a64fc325763c9caf8e38f9
Pearlsoldier <marumotodaiki@gmail.com>
2026-07-17 18:21:50 +0900

    Geminiと会話できるように変更

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-import os 
+import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from functions.tools import get_kyoto_locations, search_gourmet, search_nearby_location
 
 load_dotenv()
 
@@ -16,14 +18,14 @@ class Message(BaseModel):
     role: str
     content: str
 
+
 class ChatCompletionRequest(BaseModel):
     messages: List[Message]
 
+
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173"
-]
+origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -33,24 +35,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def hello():
-    return {"message" : "Hello,World"}
+    return {"message": "Hello,World"}
+
 
 @app.post("/v1/chat/completions")
 async def chat_completions(request: ChatCompletionRequest):
-    user_message = request.messages[-1].content
-
+    contents = []
+    for m in request.messages:
+        if m.role == "system":
+            pass
+        elif m.role == "assistant":
+            contents.append(types.Content(role="model", parts=[types.Part(text=m.content)]))
+        else:  # user
+            contents.append(types.Content(role="user", parts=[types.Part(text=m.content)]))
     response = client.models.generate_content(
-        model="gemini-2.5-flash",              
-        contents=user_message
+        model="gemini-2.5-flash",
+        contents=contents,
+        
+        
+        config=types.GenerateContentConfig(
+        tools=[get_kyoto_locations, search_gourmet, search_nearby_location],
+        system_instruction=(
+            "あなたは観光ナビゲーターです。"
+        "京都・東山エリアの神社仏閣を周遊するルートを、ユーザーの希望をもとに組み立ててください。"
+        "ユーザーが食事や飲食店を希望する場合は、search_gourmet を使って周辺の店を検索して提案してください。"
+        "ユーザーが観光施設を探したい場合は、search_nearby_locationを使ってユーザーが気になるジャンルのお店や施設を提案してください。"
         )
-    return {
-        "choices": [
-            {
-                "message": {
-                    "content": response.text
-                }
-            }
-        ]
-    }
+    ),
+    )
+    return {"choices": [{"message": {"content": response.text}}]}
