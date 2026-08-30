5f5e46c067ff42d183786a1876d3ba3521a3f71d
Pearlsoldier <marumotodaiki@gmail.com>
2026-08-30 16:29:16 +0900

    ポップアップ改修中

--- frontend/src/constants/auth.ts
+++ frontend/src/constants/auth.ts
@@ -2,6 +2,6 @@ export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
-  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+  import.meta.env.VITE_DEFAULT_API_ENDPOINT || 'https://prj-tourism-route-planner-1.onrender.com/v1/chat/completions';
 
 export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
